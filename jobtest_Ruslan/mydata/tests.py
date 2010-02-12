8a10a0659571ba25fb36bd2c19de812c45037d2a
Ruslan <root@ruslan-laptop.(none)>
2010-02-12 10:42:14 +0200

    added http view test

--- jobtest_Ruslan/mydata/tests.py
+++ jobtest_Ruslan/mydata/tests.py
@@ -1,4 +1,6 @@
-from tddspry.django import DatabaseTestCase
+from tddspry.django import DatabaseTestCase, HttpTestCase
+from django.core.urlresolvers import reverse
+
 
 from models import Mybio
 
@@ -11,8 +13,23 @@ test_contact = {"bio": "I was born January 27, 1984, Aquarius, like music, mount
 
 class TestFixture(DatabaseTestCase):
     def test_my_contact_exists(self):
+        """
+        Test if data is uploaded from fixture
+        """
         self.assert_count(Mybio, 1)
         self.assert_read(Mybio, **test_contact)
+
+class TestContactsViews(HttpTestCase):
+    def test_contacts_view(self):
+        """
+        Test if contact view contains all relevant data from fixture
+        """
+        self.go("", kwargs=test_contact)    
+        self.find(test_contact["bio"])
+        self.find(test_contact["first_name"])
+        self.find(test_contact["last_name"])
+        self.find(test_contact["contacts"])
+
 
 
 

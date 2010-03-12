fb3ab7b2ae191c1bd4989e1c797efc619dc45ea7
Ruslan <root@ruslan-laptop.(none)>
2010-03-12 12:49:54 +0200

    t9_command added model_stats functionality

---
+++ jobtest_Ruslan/mydata/management/commands/model_stats.py
@@ -0,0 +1,11 @@
+from django.core.management.base import NoArgsCommand
+from django.db.models.loading import get_models
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        """Prints all models in project and how many objects of each model is present in database 
+        """
+        for model in get_models():
+            print "%s.%s - objects in database %s"%(model._meta.app_label, 
+                                model._meta.module_name,
+                                model.objects.count())
